929e6c54b749d8580fceb36b3546967aed0070fe
Kenji79217 <47220727+Kenji79217@users.noreply.github.com>
2019-02-08 21:27:11 +0800

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,13 @@
+import random
+
+r = random.randint(1, 100)
+while True:
+	num = input('請猜數字: ')
+	num = int(num)
+	if num == r:
+		print('YA! 你猜中了! ')
+		break
+	elif num < r:
+		print('在大一點~ ')
+	elif num > r:
+		print('在小一點~ ')
