fd203d8dbb8f6d37f74fde8b705111a9b4356773
Kenji79217 <47220727+Kenji79217@users.noreply.github.com>
2019-02-08 21:52:04 +0800

    Add files via upload

--- guess.py
+++ guess.py
@@ -1,13 +1,17 @@
 import random
 
 r = random.randint(1, 100)
+conut = 0
 while True:
+	conut += 1
 	num = input('請猜數字: ')
 	num = int(num)
 	if num == r:
 		print('YA! 你猜中了! ')
+		print('這是你猜的第', conut, '次')
 		break
 	elif num < r:
 		print('在大一點~ ')
 	elif num > r:
 		print('在小一點~ ')
+	print('這是你猜的第', conut, '次')
